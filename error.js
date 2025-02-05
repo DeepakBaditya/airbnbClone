a402ba4bfe02fec8f0c14fe6a187ca1f33aabaa3
DeepakBaditya <deepakbaditya6@gmail.com>
2025-02-05 14:41:09 +0530

    learnt Validation

--- error.js
+++ error.js
@@ -1,5 +1,6 @@
 const express = require("express"); 
 const app = express();
+const ExpressError = require("./expresserror");
 app.get("/",(req,res)=>{
     console.log("hi");
 });
@@ -9,8 +10,18 @@ app.listen(8080,()=>{
 app.get("/err",(req,res)=>{
     abcd = abcd;
 });
+
+app.use((err,req,res,next)=>{
+    console.log(err.name);
+    next(err);
+});
+
 app.use((err,req,res,next)=>{
     // console.log(err);
     console.log("====ERROR====");
-    next(err); // it calls the default error handler
+    // next(err); // it calls the default error handler
 });
+
+app.get("/admin",(req,rest)=>{
+    throw new ExpressError(403,"You are not an admin");
+})
